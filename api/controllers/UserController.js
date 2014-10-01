1081283f76ba54b44b4c5090b58b9260bdecc76a
Micah Lisonbee <micahlisonbee@gmail.com>
2014-10-01 11:53:11 -0600

    Manually handled creating new user and validation, still needs client validation

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -9,7 +9,48 @@ module.exports = {
 
 	'new': function(req, res) {
 		res.view();
-	}
+	},
+
+	create: function(req, res, next) {
+
+	    var userObj = {
+	      firstName: req.param('firstName'),
+	      lastName: req.param('lastName'),
+	      email: req.param('email'),
+	      password: req.param('password')
+	    };
+
+	    if ((userObj.firstName == null || userObj.firstName == '') ||
+			(userObj.lastName == null || userObj.lastName == '') ||
+			(userObj.email == null || userObj.email == '') ||
+			(userObj.password == null || userObj.password == '')) {
+
+			console.log("missing attributes");
+			res.send({err: "All fields are required"});
+			return;
+		}
+
+	    // Create a User with the params sent from 
+	    // the sign-up form --> new.ejs
+	    User.create(userObj, function userCreated(err, user) {
+
+			if (err) {
+				console.log("Error creating user");
+				res.send({err: err});
+				return;
+			}
+
+
+			// Log user in
+			req.session.authenticated = true;
+			req.session.User = user;
+
+			console.log("User signed in.");
+			res.send({user: user});
+			return;
+
+	    });
+  	},
 	
 };
 
